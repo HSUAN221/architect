538a10be7c2146c58df363a106ac413fbbfa36da
LucaWei <lucawei@moldex3d.com>
2023-03-19 07:57:20 +0000

    add QuickSort and CountingSort

--- examples/sort_example.cpp
+++ examples/sort_example.cpp
@@ -6,9 +6,9 @@ int main(int argc, char **argv) {
     architect::MergeSortTest merge_sort_test;
     auto merge_is_passed = merge_sort_test.runMergeTest();
     if (merge_is_passed)
-        std::cout << "is same" << std::endl;
+        std::cout << "merge is same" << std::endl;
     else
-        std::cout << "isn't same" << std::endl;
+        std::cout << "merge isn't same" << std::endl;
 
     architect::MergeSort merge_sort;
     auto arr_is_sorted = merge_sort.run({99, 7, 100, 4, 5, 2});
@@ -17,5 +17,13 @@ int main(int argc, char **argv) {
 
     architect::QuickSortTest quick_sort_test;
     auto partition_is_passed = quick_sort_test.runPartitionTest();
+
+    std::vector<int> arr = {99, 7, 100, 4, 5, 2, 57, 34, 2, 2, 4, 5};
+    // architect::QuickSort().run(arr);
+
+    architect::CountingSort cs;
+    cs.run(arr);
+    for (const auto& i : arr)
+        std::cout << "CountingSort>> " << i << std::endl;
     return 0;
 }
--- src/architect/sort.hpp
+++ src/architect/sort.hpp
@@ -74,29 +74,54 @@ class MergeSortTest {
     }
 };
 
-//  in-place
+//  in-place not quick
 class QuickSort {
  private:
     int randomizePivot(int left_idx, int right_idx) {
-        std::random_device rd();
+        std::random_device rd;
         std::default_random_engine generator(rd());
-        std::uniform_int_distribution unii(left_idx, right_idx);
-        return unii(generator);
+        std::uniform_int_distribution<int> uid(left_idx, right_idx);
+        return uid(generator);
     }
 
     int partition(std::vector<int>& arr, int left_idx, int right_idx) {
-        auto pivot = randomizePivot();
-        std::swap(arr[pivot], arr[right_idx);
+        auto pivot_idx = randomizePivot(left_idx, right_idx);
+        std::swap(arr[pivot_idx], arr[right_idx]);
+        auto pivot = arr[right_idx];
         auto i = left_idx;
-        for (size_t j = 0; j < right_idx - 1; ++j) {
-            if (arr[j] < pivot) {
-                // std::swap()
+        for (int j = left_idx; j < right_idx; ++j) {
+            if (arr[j] < pivot)
+                std::swap(arr[i++], arr[j]);
+        }
+        std::swap(arr[i], arr[right_idx]);
+        return i;
+    }
+
+    int partition1(std::vector<int>& arr, int left_idx, int right_idx) {
+        auto pivot_idx = randomizePivot(left_idx, right_idx);
+        auto i = left_idx, j = right_idx;
+        while (i <= j) {
+            while (arr[i] < arr[pivot_idx]) ++i;
+            while (arr[j] > arr[pivot_idx]) --j;
+            if (i <= j) {
+                std::swap(arr[i++], arr[j--]);
             }
         }
-        return pivot;
+
+        return i - 1;
+    }
+
+    void helper(std::vector<int>& arr, int left_idx, int right_idx) {
+        if (left_idx >= right_idx) return;
+        auto pivot = partition(arr, left_idx, right_idx);
+        helper(arr, left_idx, pivot - 1);
+        helper(arr, pivot + 1, right_idx);
     }
 
  public:
+    void run(std::vector<int>& arr) {
+        helper(arr, 0, static_cast<int>(arr.size())-1);
+    }
 
     friend class QuickSortTest;
 };
@@ -109,13 +134,41 @@ class QuickSortTest {
     QuickSortTest() = default;
 
     bool runPartitionTest() {
-        std::vector<int> expected = {1, 2, 3, 4, 5};
-        std::vector<int> arr = {4, 5, 3, 1, 2};
-        auto pivot = qs_.partition(arr, 0, static_cast<int>(arr.size())-1);
+        std::vector<int> expected = {4, 5, 6, 7, 10};
+        std::vector<int> arr = {4, 5, 10, 6, 7};
+        auto pivot = qs_.partition1(arr, 0, static_cast<int>(arr.size())-1);
         std::cout << "pivot= " << pivot << std::endl;
-        // for (const auto& i : result)
-        //     std::cout << i << std::endl;
+        for (const auto& i : arr)
+            std::cout << i << std::endl;
         return arr == expected;
+    }
+};
+
+
+// best solution in leetcode
+class CountingSort {
+ public:
+    CountingSort() = default;
+
+    ~CountingSort() = default;
+
+    void run(std::vector<int>& arr) {
+        std::unordered_map<int, int> counts;
+        auto min_value = *std::min_element(arr.begin(), arr.end());
+        auto max_value = *std::max_element(arr.begin(), arr.end());
+
+        for (const auto& e : arr)
+            counts[e]++;
+
+        int index = 0;
+        for (int val = min_value; val <= max_value; ++val) {
+            if (counts.find(val) != counts.end()) {
+                while (counts[val] > 0) {
+                    arr[index++] = val;
+                    counts[val]--;
+                }
+            }
+        }
     }
 };
 
