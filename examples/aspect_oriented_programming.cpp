9f715c072681c61e1fdd7e7014f045e200860920
LucaWei <lucawei@moldex3d.com>
2023-08-10 00:17:11 +0800

    aop+DynamicProxy

--- examples/aspect_oriented_programming.cpp
+++ examples/aspect_oriented_programming.cpp
@@ -1,41 +1,98 @@
 // Copyright (c) 2022 LucaWei
 #include <architect/architect.hpp>
-
-namespace util {
-class Item {
-    friend class Queue;
-    Item* next;
-
+class PhaseSolverInterface {
  public:
-    Item() : next(nullptr){}
+    virtual void before() = 0;
+    virtual void run() = 0;
+    virtual void after() = 0;
 };
 
-class Queue {
-    Item* first;
-    Item* last;
+class VofSolver : public PhaseSolverInterface {
  public:
-    Queue() : first(nullptr), last(nullptr) {}
-    void enqueue(Item* item) {
-        if (last) {
-            last->next = item;
-            last = item;
-        } else {
-            last = first = item;
-        }
+    VofSolver() = default;
+    virtual ~VofSolver() = default;
+
+    void before() override {
+        std::cout
+          << __func__
+          << std::endl;
     }
-    Item* dequeue() {
-        Item* res = first;
-        if (first == last) {
-            first = last = nullptr;
-        } else {
-            first = first->next;
-        }
-        return res;
+
+    void run() override {
+        std::cout
+          << __func__
+          << std::endl;
+    }
+
+    void after() override {
+        std::cout
+          << __func__
+          << std::endl;
     }
 };
-}  // namespace util
+
+class Logger : public aop::AspectInterface {
+ public:
+    Logger() = default;
+    virtual ~Logger() = default;
+
+    template<typename... Args>
+    void before(Args&&... args) {
+        std::cout
+          << "[logger before]"
+          << std::endl;
+    }
+
+    template<typename... Args>
+    void after(Args&&... args) {
+        std::cout
+          << "[logger after]"
+          << std::endl;
+    }
+};
+
+class VofSolverProxy : public PhaseSolverInterface {
+ private:
+    std::shared_ptr<VofSolver> vof_solver_{nullptr};
+
+ public:
+    explicit VofSolverProxy(std::shared_ptr<VofSolver>&& vof_solver)
+    : vof_solver_(vof_solver) {
+    }
+
+    virtual ~VofSolverProxy() = default;
+
+    VofSolverProxy() = delete;
+
+    void before() override {
+        aop::Invoke<Logger>(
+          Logger(),
+          &VofSolver::before,
+          vof_solver_);
+    }
+
+    void run() override {
+        aop::Invoke<Logger>(
+          Logger(),
+          &VofSolver::run,
+          vof_solver_);
+    }
+
+    void after() override {
+        aop::Invoke<Logger>(
+          Logger(),
+          &VofSolver::after,
+          vof_solver_);
+    }
+};
 
 
 int main(int argc, char **argv) {
+    auto vof_solver_proxy =
+      std::make_shared<VofSolverProxy>(
+      std::make_shared<VofSolver>());
+    vof_solver_proxy->before();
+    vof_solver_proxy->run();
+    vof_solver_proxy->run();
     return 0;
 }
--- src/architect/aop.hpp
+++ src/architect/aop.hpp
@@ -1,42 +1,96 @@
 #ifndef SRC_ARCHITECT_AOP_HPP_
 #define SRC_ARCHITECT_AOP_HPP_
-
 #include <functional>
 #include <map>
+namespace aop {
+class NonCopyable {
+public:
+    NonCopyable() = default;
 
-namespace arch {
+    ~NonCopyable() = default;
 
+    NonCopyable(const NonCopyable &) = delete;
 
-// forward declarition. aopfy will reconstruct the series of type after
-// iteration, and put it into fulltype_t;
-template <typename _AopBase, template <typename> class... __Aspects>
-struct aopfy;
-
-// parameter base wrap.Only if we have a base with the template parameters, we
-// can reconstruct the fulltype.
-template <typename _Base, template <typename> class... __Aspects>
-struct aopbase {
-    // reference_t will keep the same during iteration.
-    using reference_t = aopfy<aopbase, __Aspects...>;
-    using base_t = _Base;
-};
-
-// iteration template defination.
-template <typename _AopBase, template <typename> class __FirstAspects,
-          template <typename> class... __RestAspects>
-struct aopfy<_AopBase, __FirstAspects, __RestAspects...> {
-    using fulltype_t = __FirstAspects<
-      typename aopfy<_AopBase, __RestAspects...>::fulltype_t>;
-    using this_t = typename _AopBase::base_t;
-};
-
-// end of iteration tempalte defination.
-template <typename _AopBase> struct aopfy<_AopBase> {
-    using fulltype_t = typename _AopBase::reference_t;
-    using this_t = typename _AopBase::base_t;
+    NonCopyable &operator=(const NonCopyable &) = delete;
 };
 
 
-} // end of namespace qaop
+class AspectInterface {
+ public:
+    AspectInterface() = default;
+
+    ~AspectInterface() = default;
+
+    template<typename... Args>
+    void before(Args&&... args) {}
+
+    template<typename... Args>
+    void after(Args&&... args) {}
+};
+
+template<typename F, typename... AP>
+class Aspects : public NonCopyable {
+ private:
+    F func_;
+    std::tuple<AP...> aps_;
+
+ public:
+    Aspects(F&& func, AP&&... aps)
+    : func_(std::forward<F>(func)),
+    aps_(std::make_tuple(std::forward<AP>(aps)...)) {}
+
+    ~Aspects() = default;
+
+    template<std::size_t I = 0, typename... Args>
+    typename std::enable_if<I == sizeof...(AP)>::type
+    Invoke(Args&&... args) {
+        func_(std::forward<Args>(args)...);
+    }
+
+    template<std::size_t I = 0, typename... Args>
+    typename std::enable_if<I < sizeof...(AP)>::type
+    Invoke(Args&&... args) {
+#ifndef NDEBUG
+        static_assert(std::is_base_of<AspectInterface,
+          typename std::decay<decltype(std::get<I>(aps_))>::type>::value,
+          "Aspect object must derives from BaseAspect.\n");
+#endif
+        std::get<I>(aps_).before(std::forward<Args>(args)...);
+        Invoke<I + 1>(std::forward<Args>(args)...);
+        std::get<I>(aps_).after(std::forward<Args>(args)...);
+    }
+};
+
+
+/// for user
+template <typename... AP, typename F, typename... Args>
+typename std::enable_if<!std::is_member_function_pointer<F>::value>::type
+Invoke(AP&&... aps, F&& func, Args&&... args) {
+    Aspects<F, AP...>(
+      std::forward<F>(func),
+      std::forward<AP>(aps)...).Invoke(std::forward<Args>(args)...);
+}
+
+template <typename... AP, typename F, typename TP, typename... Args>
+typename std::enable_if<std::is_member_function_pointer<F>::value>::type
+Invoke(AP &&... aps, F &&func, TP *target, Args &&... args) {
+    auto bind = [&](Args &&... args) -> void {
+        (target->*std::forward<F>(func))(std::forward<Args>(args)...);
+    };
+
+    Invoke<AP...>(std::forward<AP>(aps)..., bind, std::forward<Args>(args)...);
+}
+
+template <typename... AP, typename F, typename TP, typename... Args>
+typename std::enable_if<std::is_member_function_pointer<F>::value>::type
+Invoke(AP &&... aps, F &&func, std::shared_ptr<TP> target, Args &&... args) {
+    auto bind = [&](Args &&... args) -> void {
+        ((target.get())->*std::forward<F>(func))(std::forward<Args>(args)...);
+    };
+
+    Invoke<AP...>(std::forward<AP>(aps)..., bind, std::forward<Args>(args)...);
+}
+
+} // end of namespace aop
 
 #endif // SRC_ARCHITECT_AOP_HPP_
